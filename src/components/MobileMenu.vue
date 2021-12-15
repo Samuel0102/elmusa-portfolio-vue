fa880df86a43b834388937b386fd783d6e83acd9
Samuel0102 <damu.comsa@gmail.com>
2021-12-15 16:02:47 -0300

    Menu Mobile Responsivo, Animado e Finalizado

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="relative sm:hidden">
     <i
+      v-if="this.$route.name === 'Index'"
       :class="[{ 'fas fa-times': isOpen }, 'fas fa-bars']"
       id="hamburguer"
-      @click="openMenu"
+      @click="isOpen = !isOpen"
     ></i>
 
-    <div class="fixed z-20 w-full h-full" id="overlay" v-if="isOpen">
-      <nav class="mt-14 w-72 h-96 fixed left-10 bg-white">
-        <ul class="text-left px-5 py-4 uppercase text-sm font-medium">
-          <li><a href="#home">Home</a></li>
-          <li class="py-4"><a href="#about">Sobre</a></li>
-          <li><a href="#services">Serviços</a></li>
-          <li class="py-4"><a href="#portfolio">Portfólio</a></li>
-          <li><a href="#contact">Contato</a></li>
-        </ul>
-      </nav>
-    </div>
+    <transition name="slide-fade">
+      <div id="overlay" v-if="isOpen">
+        <nav class="nav">
+          <ul class="ul">
+            <li><a href="#home">Home</a></li>
+            <li class="py-4"><a href="#about">Sobre</a></li>
+            <li><a href="#services">Serviços</a></li>
+            <li class="py-4"><a href="#portfolio">Portfólio</a></li>
+            <li><a href="#contact">Contato</a></li>
+          </ul>
+        </nav>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -25,28 +28,48 @@ export default {
   data() {
     return {
       isOpen: false,
-    }
-  },
-  methods: {
-    openMenu() {
-      this.isOpen = !this.isOpen;
-    },
+    };
   },
 };
 </script>
 
 <style scoped>
-ul li {
+.ul li {
   @apply hover:text-green-600;
 }
 
+.nav {
+  @apply mt-20 w-9/12 h-4/6 mx-auto bg-white shadow-lg rounded-sm;
+}
+
+.ul {
+  @apply text-left px-5 py-4 uppercase text-base font-medium text-gray-800;
+}
+
 #overlay {
-  background: rgba(0, 0, 0, 0.5);
+  @apply fixed w-full z-20 mx-auto h-screen bg-black bg-opacity-70;
 }
 
 #hamburguer {
   @apply leading-6 text-white text-xl
                 cursor-pointer fixed z-30 py-5 right-0
                 pr-6 md:pr-10 lg:pr-20;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s ease-out;
+}
+
+.slide-fade-enter-from {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+.slide-fade-leave-to {
+  opacity: 0;
 }
 </style>
