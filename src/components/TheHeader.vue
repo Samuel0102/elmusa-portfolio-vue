56539c0e17a03f172d3c1e061937a4acc1141360
Samuel0102 <damu.comsa@gmail.com>
2021-12-15 16:39:14 -0300

    Header Responsivo, Animado e Finalizado

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <header>
-    <h1 class="text-white leading-6 text-xl" id="port-title">Elmusa</h1>
-    
+  <header class="header">
+    <h1 class="leading-6 text-xl font-medium" id="port-title">Elmusa</h1>
+
     <nav v-if="isOnIndex">
-      <ul class="text-white hidden sm:block">
+      <ul class="hidden sm:block ul space-x-6">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">Sobre</a></li>
         <li><a href="#services">Serviços</a></li>
@@ -12,25 +12,22 @@
       </ul>
     </nav>
 
-    <div v-else> 
-        <router-link to="/" class="text-white">
-          <i class="fas fa-arrow-left mr-3"></i>Back to Index
-        </router-link>
+    <div v-else>
+      <router-link to="/" class="text-white">
+        <i class="fas fa-arrow-left mr-3"></i>Back to Index
+      </router-link>
     </div>
-
   </header>
 </template>
 
 <script>
 export default {
   computed: {
-    isOnIndex(){
-      return this.$route.name === "Index"
-    }
-  }
-
-}
-
+    isOnIndex() {
+      return this.$route.name === "Index";
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -43,12 +40,30 @@ export default {
   font-family: Moon;
 }
 
-header{
-  @apply flex items-baseline justify-between px-6 md:px-10 lg:px-20 py-5 bg-black sticky top-0 w-full z-10;
+.header {
+  @apply text-white flex items-baseline justify-between
+           px-6 md:px-10 lg:px-20 py-5 bg-black sticky
+           top-0 w-full z-10;
 }
 
-ul li{
-  @apply inline px-2 font-medium uppercase;
+.ul li {
+  @apply inline font-medium relative text-lg;
 }
 
+.ul li:hover:before {
+  animation: underline 1s both;
+  content: "";
+  bottom: 0;
+  border: 1px solid white;
+  position: absolute;
+}
+
+@keyframes underline {
+  from {
+    width: 0;
+  }
+  to {
+    width: 99.8%;
+  }
+}
 </style>
